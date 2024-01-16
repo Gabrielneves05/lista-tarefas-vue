7531323809fc1fedee209d620fa826fde790a7f4
Gabriel Neves <129914892+Gabrielneves05@users.noreply.github.com>
2024-01-16 01:53:46 -0300

    Projeto finalizado

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,10 +1,62 @@
 <template>
   <div class="container mt-2">
-    <b-card title="Título">
-      <b-card-text>Descrição</b-card-text>
+    <div v-for="(task, index) in tasks" :key="index">
+      <b-card :title="task.subject" class="mb-3">
+        <b-card-text>{{ task.description }}</b-card-text>
 
-      <b-button variant="outline-secondary" class="mr-2">Editar</b-button>
-      <b-button variant="outline-danger" class="mr-2">Excluir</b-button>
-    </b-card>
+        <b-button variant="outline-secondary" class="mr-2" @click="edit(index)">Editar</b-button>
+        <b-button variant="outline-danger" class="mr-2" @click="remove(task, index)">Excluir</b-button>
+      </b-card>
+    </div>
+
+    <b-modal ref="modalRemove" hide-footer title="Exclusão de tarefa">
+      <div class="d-block text-center">
+        <p>Deseja realmente excluir a tarefa "{{ taskSelected.subject }}"?</p>
+      </div>
+      <div class="mt-3 d-flex justify-content-end">
+        <b-button variant="outline-secondary" class="mr-2" @click="hideModal">Cancelar</b-button>
+        <b-button variant="outline-danger" class="mr-2" @click="confirmRemoveTask">Excluir</b-button>
+      </div>
+    </b-modal>
   </div>
 </template>
+
+<script>
+  export default {
+    name: "List",
+
+    data() {
+      return {
+        tasks: [],
+        taskSelected: []
+      }
+    },
+
+    created() {
+      this.tasks = (localStorage.getItem("tasks")) ? JSON.parse(localStorage.getItem("tasks")) : [];
+    },
+
+    methods: {
+      edit(index) {
+        this.$router.push({ name: "form", params: { index } })
+      },
+
+      remove(task, index) {
+        this.taskSelected = task;
+        this.taskSelected.index = index;
+        this.$refs.modalRemove.show();
+      },
+
+      hideModal() {
+        this.$refs.modalRemove.hide();
+      },
+
+      confirmRemoveTask() {
+        this.tasks.splice(this.taskSelected.index, 1);
+        localStorage.setItem("tasks", JSON.stringify(this.tasks));
+        this.hideModal();
+      }
+    }
+  }
+
+</script>
